f75e67bbdf8091a988496a2528fa633fc57def9a
DevinT7 <136363837+DevinT7@users.noreply.github.com>
2026-07-15 12:49:08 -0500

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+       Arrays.sort(nums);
+       boolean x = false;
+       for (int i = 0; i < nums.length-1; i++){
+        if (nums[i] == nums[i+1]){
+            //temp = nums[i+1];
+            x = true;
+        }
+       }
+       return x; 
+    }
+}
